663ff3d8c802eb62c4c9b512ed9ea86650fe8ce6
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-06 23:29:14 +0530

    Started with header and styled it

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,44 @@
+import { Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
 import React from 'react'
 import styled from "styled-components";
 
 function Header() {
     return (
-        <HeaderLeft>Hello bsdk</HeaderLeft>
+        <HeaderContainer>
+            <HeaderLeft>
+                <HeaderAvatar />
+                <AccessTimeIcon />
+            </HeaderLeft>
+
+            <HeaderMid>
+                <SearchIcon />
+                <input placeholder="Search" />
+            </HeaderMid>
+
+            <HeaderRight></HeaderRight>
+        </HeaderContainer>
     )
 }
 
 export default Header
 
+const HeaderContainer = styled.div`
+    color: white;
+    background-color: var(--slack-color);
+    display: flex;
+    position: fixed;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 0px;
+`;
+
 const HeaderLeft = styled.div`
-    color: black;
-    font-size: large;
+    flex: 0.3;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0.7em 1.2em;
 `;
