8267c73d126c8bc5b166de12bb3fc4d6f16d71d9
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-07 15:27:12 +0530

    Started with appbody and in it sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 import './App.css';
 import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-       <Header />
+      <Header />
+      <AppBody>
+        <Sidebar />
+      </AppBody>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div``;
