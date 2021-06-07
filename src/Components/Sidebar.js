372b61e24e08a6468a9869b5284aba358738d22b
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-07 16:56:18 +0530

    Passed props and started with sidebarop

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -7,6 +7,10 @@ import InsertCommentIcon from '@material-ui/icons/InsertComment'
 import InboxIcon from '@material-ui/icons/Inbox'
 import DraftsIcon from '@material-ui/icons/Drafts'
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt'
+import AppsIcon from '@material-ui/icons/Apps'
+import FileCopyIcon from '@material-ui/icons/FileCopy'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
 
 function Sidebar() {
     return (
---
+++ src/Components/SidebarOp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function SidebarOp() {
+    return (
+       <SidebarOpContainer></SidebarOpContainer>
+    )
+}
+
+export default SidebarOp
+
+const SidebarOpContainer = styled.div``;
