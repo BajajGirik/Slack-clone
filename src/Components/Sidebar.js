d78f8f87d0867f1106662de9e3f69e4bed02304e
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-07 15:56:13 +0530

    Styling top of sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,4 +1,5 @@
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
+import CreateIcon from '@material-ui/icons/Create'
 import React from 'react'
 import styled from 'styled-components'
 
@@ -10,6 +11,7 @@ function Sidebar() {
                     <h2>USICT HQ</h2>
                     <h3><FiberManualRecordIcon /> Girik Bajaj</h3>
                 </SidebarText>
+                <CreateIcon />
             </SidebarTop>
         </SidebarContainer>
     )
@@ -18,9 +20,21 @@ function Sidebar() {
 export default Sidebar
 
 const SidebarContainer = styled.div`
+    color: white;
     background-color: var(--slack-color);
     flex: 0.3;
+    margin-top: 70px;
+    max-width: 260px;
 `;
-const SidebarTop = styled.div``;
+const SidebarTop = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 1em;
+    
+    > .MuiSvgIcon-root {
+    
+    }
+
+`;
 const SidebarText = styled.div``;
 
