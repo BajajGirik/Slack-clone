87cf68fbe347ee50ca454c9226dab7ad4c470dc6
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-08 21:46:21 +0530

    Implemented signIn

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,13 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
 import styled from 'styled-components'
+import { auth, provider } from '../firebase';
 
 function Login() {
     const signIn = e => {
-
+        auth.signInWithPopup(provider).catch(
+            (error) => alert(error.message)
+        );
     }
 
     return (
