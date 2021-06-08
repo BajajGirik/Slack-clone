d4ed22a7244d9d72978feaa612aaeb2024f79883
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-08 13:34:37 +0530

    Using states to save input and refering to db

--- src/Components/ChatInput.js
+++ src/Components/ChatInput.js
@@ -1,16 +1,33 @@
 import { Button } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { db } from '../firebase';
+import firebase from 'firebase'
 
-function ChatInput({ }) {
+function ChatInput({channelName, channelId }) {
+    const [input, setInput] = useState('');
+    
+    const changeIt = e => {
+        setInput(e.target.value);
+    }
+
     const sendMessage = e => {
         e.preventDefault();
+
+        if (!channelId) {
+            return false;
+        }
+
+        db.collection('rooms').doc(channelId).collection('messages').add({
+            message: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        });
     }
 
     return (
         <ChatInputContainer>
             <form>
-                <input placeholder={`Message #ROOM`} />
+                <input value={input} onChange={changeIt} placeholder={`Message #ROOM`} />
                 <Button type='submit' onClick={sendMessage} hidden>Send</Button>
             </form>    
         </ChatInputContainer>
