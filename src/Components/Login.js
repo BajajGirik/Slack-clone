3a0b69277f4cf473cb55ec890b6154e0839cbac7
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-08 21:40:47 +0530

    Styling login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,48 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
+import styled from 'styled-components'
 
 function Login() {
+    const signIn = e => {
+        
+    }
+
     return (
-        <div>
-            
-        </div>
+        <LoginContainer>
+            <LoginMid>
+                <img src="https://yt3.ggpht.com/ytc/AAUvwnhZtcTvJEkvuZMdTzjhPLvZGIQSo9nel4btx7j9rg=s900-c-k-c0x00ffffff-no-rj" alt="" />
+                <h2>Slack</h2>
+                <Button type='submit' onClick={signIn}>Sign In</Button>
+            </LoginMid>
+        </LoginContainer>
     )
 }
 
 export default Login
+
+const LoginContainer = styled.div`
+    background-color: #f8f8f8;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const LoginMid = styled.div`
+    text-align: center;
+    background-color: #fff;
+    padding: 10em;
+    border-radius: 1em;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12),
+    0 1px 2px rgba(0, 0, 0, 0.24);
+
+    > img {
+        object-fit: contain;
+        height: 100px;
+    }
+
+    >button {
+        margin-top: 2em;
+    }
+
+`;
