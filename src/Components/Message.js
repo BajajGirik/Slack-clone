b243ec05aedcb8416a7a8886c9e8f5af8f1e2675
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-08 19:26:23 +0530

    Started styling messages

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import styled from 'styled-components';
 
-function Message() {
+function Message({message, timestamp, user, userImage}) {
     return (
         <MessageContainer>
-            
+            <img src={userImage} alt='' />
+            <MessageInfo>
+                <h4>
+                    {user} {' '}
+                    <span>{new Date(timestamp?.toDate()).toLocaleTimeString()}</span>
+                    <p>{message}</p>
+                </h4>
+            </MessageInfo>
         </MessageContainer>
     )
 }
 
 export default Message
 
-const MessageContainer = styled.div``;
+const MessageContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 20px;
+
+    >img {
+        object-fit: contain;
+        height: 50px;
+        border-radius: 8px;
+    }
+
+`;
+const MessageInfo = styled.div`
+    padding-left: 10px;
+
+`;
