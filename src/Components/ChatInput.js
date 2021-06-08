e10de9c2e6c407ece41f1884daed6ceb49a08509
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-08 13:24:00 +0530

    Styled chat input

--- src/Components/ChatInput.js
+++ src/Components/ChatInput.js
@@ -3,17 +3,40 @@ import React from 'react'
 import styled from 'styled-components'
 
 function ChatInput({ }) {
-    const sendMessage = () => {
+    const sendMessage = e => {
+        e.preventDefault();
     }
-    
+
     return (
         <ChatInputContainer>
-            <input placeholder={`Message #ROOM`} />
-            <Button type='submit' onClick={sendMessage} hidden>Send</Button>
+            <form>
+                <input placeholder={`Message #ROOM`} />
+                <Button type='submit' onClick={sendMessage} hidden>Send</Button>
+            </form>    
         </ChatInputContainer>
     )
 }
 
 export default ChatInput
 
-const ChatInputContainer = styled.div``;
+const ChatInputContainer = styled.div`
+    
+    >form {
+        display: flex;
+        justify-content: center;
+    }
+
+    >form > input {
+        outline: none;
+        padding:1em;
+        position: fixed;
+        bottom: 30px;
+        width: 60%;
+        border: solid 1px gray;
+        border-radius: 5px;
+    }
+
+    >form > button {
+        display: none !important;
+    }
+`;
